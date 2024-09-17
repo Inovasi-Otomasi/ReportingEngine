cc6d5b446b5048ad64ec82964b34639e440564cf
michaeljuan97 <82603969+asanihasan@users.noreply.github.com>
2024-09-17 11:03:03 +0700

    report template dropdown

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -83,7 +83,7 @@ class Dashboard extends CI_Controller {
 					$this->load->view('modal/editReport', $return);
 				} else {
 					$return['template'] = $this->db->get('template')->result();
-					$this->load->view('modal/newReport');
+					$this->load->view('modal/newReport', $return);
 				}
 			  	break;
 			case 'editor':
